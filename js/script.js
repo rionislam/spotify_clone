10c7a7cdffd41099c5d1b4ea1b14f76993d6ca37
Mazidul Islam Rion <87961108+rionislam@users.noreply.github.com>
2021-12-14 01:10:34 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ songItems.forEach((element, i) => {
   element.addEventListener('click', () => {
     audioElement.src = songs[i].filePath;
     audioElement.play();
-    songPlayBtn.src = '../svg/pause-circle-solid.svg';
+    songPlayBtn.src = './svg/pause-circle-solid.svg';
     document.getElementById('cover').src = songs[i].coverPath;
     document.getElementById('songName').innerHTML = songs[i].songName;
     document.getElementById('duration').innerHTML = songs[i].duration;
@@ -157,7 +157,7 @@ mySoundBar.addEventListener('input', () => {
 songPlayBtn.addEventListener('click', () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
-    songPlayBtn.src = '../svg/pause-circle-solid.svg';
+    songPlayBtn.src = './svg/pause-circle-solid.svg';
     if (audioElement.volume == 0) {
       audioElement.volume = 0.1;
       document.getElementById('volume').style.width = '10%';
@@ -186,7 +186,7 @@ volume.addEventListener('click', () => {
     volume.innerHTML =
       '<path fill="currentColor" d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zM461.64 256l45.64-45.64c6.3-6.3 6.3-16.52 0-22.82l-22.82-22.82c-6.3-6.3-16.52-6.3-22.82 0L416 210.36l-45.64-45.64c-6.3-6.3-16.52-6.3-22.82 0l-22.82 22.82c-6.3 6.3-6.3 16.52 0 22.82L370.36 256l-45.63 45.63c-6.3 6.3-6.3 16.52 0 22.82l22.82 22.82c6.3 6.3 16.52 6.3 22.82 0L416 301.64l45.64 45.64c6.3 6.3 16.52 6.3 22.82 0l22.82-22.82c6.3-6.3 6.3-16.52 0-22.82L461.64 256z"></path>';
     audioElement.pause();
-    songPlayBtn.src = '../svg/play-circle-solid.svg';
+    songPlayBtn.src = './svg/play-circle-solid.svg';
   }
 });
 
